a8314d9ac65a3525b9061030c85f7106071a3be5
Antonio Andelic <antonio@clickhouse.com>
2024-11-19 11:40:41 +0100

    Fix

--- src/handle_client_request.cxx
+++ src/handle_client_request.cxx
@@ -67,7 +67,7 @@ ptr<resp_msg> raft_server::handle_leader_status_req(req_msg& req) {
         case raft_params::dual_mutex:
         default: {
             // TODO: Use RW lock here.
-            auto_lock(cli_lock_);
+            recur_lock(cli_lock_);
             return get_leader_status();
         }
     }
